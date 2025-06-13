7b2fed0842bfaa1d6366b85c555e750fc3c8f0bb
Abhinav Yadav <mr.kumarabhinavyadav@gmail.com>
2025-06-13 11:30:41 +0530

    Update database name to 'vishalroadlineprod' in MongoDB connection

--- backend/db.py
+++ backend/db.py
@@ -4,5 +4,5 @@ from motor.motor_asyncio import AsyncIOMotorClient
 load_dotenv() 
 MONGO_DETAILS = os.getenv("MONGO_DETAILS")  # Your MongoDB URI
 client = AsyncIOMotorClient(MONGO_DETAILS)
-database = client.vishalroadline  # Database name
+database = client.vishalroadlineprod  # Database name
 invoice_collection = database.get_collection("invoices")  # Collection name
